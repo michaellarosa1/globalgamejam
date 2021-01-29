630e48da818627e890a95cd2d23d248be2188035
DavideWu <56259661+DavideWu@users.noreply.github.com>
2021-01-29 21:37:00 +0100

    OSC / Sender / Recever

---
+++ global gamejam/Assets/Scripts/Recever.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recever : MonoBehaviour
+{
+    public OSC MyOsc;
+    public Transform transf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyOsc.SetAddressHandler("/SyncTransform",ReceveSyncTransform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ReceveSyncTransform(OscMessage inputMessage)
+    {
+        transf.localPosition = new Vector3(inputMessage.GetFloat(0),inputMessage.GetFloat(1),inputMessage.GetFloat(2));
+        transf.localRotation = new Quaternion(inputMessage.GetFloat(3),inputMessage.GetFloat(4),inputMessage.GetFloat(5),inputMessage.GetFloat(6));
+    }
+}
---
+++ global gamejam/Assets/Scripts/Sender.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sender : MonoBehaviour
+{
+    public OSC MyOsc;
+
+    public Transform transf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OscMessage tempMessageToSend = new OscMessage();
+        tempMessageToSend.address = "/SyncTransform";
+        tempMessageToSend.values.Add(transf.localPosition.x);
+        tempMessageToSend.values.Add(transf.localPosition.y);
+        tempMessageToSend.values.Add(transf.localPosition.z);
+
+        tempMessageToSend.values.Add(transf.localRotation.x);
+        tempMessageToSend.values.Add(transf.localRotation.y);
+        tempMessageToSend.values.Add(transf.localRotation.z);
+        tempMessageToSend.values.Add(transf.localRotation.w);
+
+        MyOsc.Send(tempMessageToSend);
+    }
+}
