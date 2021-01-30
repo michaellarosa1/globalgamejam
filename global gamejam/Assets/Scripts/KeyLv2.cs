bf6383377b57a8afd358da7bb1af8bb6d674f612
DavideWu <56259661+DavideWu@users.noreply.github.com>
2021-01-30 16:31:55 +0100

    Lv2 ManopolaGame

---
+++ global gamejam/Assets/Scripts/KeyLv2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLv2 : MonoBehaviour
+{
+    public Animator AnimatorToActive;
+    public string NameOfParameterBool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void keyIn()
+    {
+        AnimatorToActive.SetBool(NameOfParameterBool, true);
+    }
+
+    public void keyOff()
+    {
+        AnimatorToActive.SetBool(NameOfParameterBool, false);
+    }
+}
