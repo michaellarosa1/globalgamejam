69fdc87e1941554e79dbbfee7a1560f165e12678
EdoardoPomaStud <56259618+EdoardoPomaStud@users.noreply.github.com>
2021-01-30 10:17:54 +0100

    manopola

---
+++ global gamejam/Assets/Scripts/KeyControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyControl : MonoBehaviour
+{
+    public Animator AnimatorToActive;
+    public string NameOfParameterBool;
+
+    private void OnEnable()
+    {
+        AnimatorToActive.SetBool(NameOfParameterBool, true);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
