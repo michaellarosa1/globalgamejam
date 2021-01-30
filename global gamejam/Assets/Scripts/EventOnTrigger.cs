b8a9618e14e2657e7e32299f473fe358a0b918b9
DavideWu <56259661+DavideWu@users.noreply.github.com>
2021-01-30 03:25:32 +0100

    Manopola / anim / Button

---
+++ global gamejam/Assets/Scripts/EventOnTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnTrigger : MonoBehaviour
+{
+    public UnityEvent action;
+    public UnityEvent actionUscita;
+    public string TriggerTag;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(TriggerTag))
+        {
+            if (action != null)action.Invoke();   
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag(TriggerTag))
+        {
+            if (action != null)actionUscita.Invoke();
+        }
+    }
+}
