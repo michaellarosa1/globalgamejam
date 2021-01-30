6affda9d61fb5c60f0300e3025fafad9e031c572
michaellaros <56259544+michaellaros@users.noreply.github.com>
2021-01-31 00:42:09 +0100

    credits

---
+++ global gamejam/Assets/Scripts/GameENDER.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameENDER : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("menu");
+        Debug.Log("menu");
+        
+    }
+
+    
+}
